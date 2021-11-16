8e0c9744aac94734eeabdfc0c161b7ca8c7641b9
Louazri Hassan <hassan.louazri@bordeaux-inp.fr>
2021-11-16 19:17:15 +0100

    fix repertories

--- Etape 1/Planche.java
+++
@@ -1,8 +0,0 @@
-public class Planche extends Wood
-{
-    
-    Planche(int id, int number,String date, double price,Dimension dimension) {
-        super(id,number,date,price,dimension);
-    }
-
-}
